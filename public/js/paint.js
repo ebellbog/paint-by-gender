0bb962c947d63f372b3b931c7ddedcc6f61a2cfe
Elana Bogdan <e.bell.bogdan@gmail.com>
2018-02-20 18:09:55 -0500

    improve analysis; add progress meter; draw level 1

--- public/js/paint.js
+++ public/js/paint.js
@@ -5,6 +5,10 @@ function midPointBtw(p1, p2) {
   };
 }
 
+function getDistance(p1, p2) {
+  return Math.pow(Math.pow(p1.x-p2.x,2)+Math.pow(p1.y-p2.y,2), 0.5);
+}
+
 function getCursorPos(canvas, e) {
   var rect = canvas.getBoundingClientRect();
   return {
@@ -13,19 +17,24 @@ function getCursorPos(canvas, e) {
   };
 }
 
+function getContext($canvas) {
+  if (!$canvas) $canvas = $('#game');
+  var ctx = $canvas[0].getContext('2d');
+  return ctx;
+}
+
 function getPercentPainted() {
   var ctx = getContext();
   var imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height).data;
   var white = 0, painted = 0;
-  var reducer = function(accumulator, currentValue){return accumulator+currentValue};
   
   for (var i = 0; i < imageData.length; i+=4) {
-    var pixel = imageData.slice(i, i+3);
-    var sum = pixel.reduce(reducer);
-    if (sum > 0) {
-      painted++;
-    } else {
+    if (imageData[i] == 255) {
       white++;
+    } else if (imageData[i] == 132) {
+      continue;
+    } else {
+      painted++;
     }
   }
   return painted/(painted+white);
@@ -33,26 +42,47 @@ function getPercentPainted() {
 
 function updatePercentPainted() {
   var ctx = getContext();
-  var value = getPercentPainted(ctx)*100;
-  var rounded = Math.round(value*100)/100;
-  $('#percent-painted').html(rounded+'%');
+  var percent = getPercentPainted(ctx);
+
+  var $slider = $('.slider-outline').first();
+  var height = $slider.height();
+  var $fill = $('.slider-fill');
+  $fill.animate({height: height*percent}, 100, 'linear');
 }
 
-function getContext($canvas) {
-  if (!$canvas) $canvas = $('#game');
-  var ctx = $canvas[0].getContext('2d');
-  return ctx;
+function updatePercentAsync() {
+  setTimeout(updatePercentPainted, 0);
+}
+
+function setupLevel(level) {
+  var ctx = getContext();
+  switch(level) {
+    case 1:
+      ctx.fillStyle = 'rgb(132, 189, 250)';
+      ctx.fillRect(0, 0, 500, 500);
+      ctx.fillStyle = 'white';
+      ctx.beginPath();
+      ctx.arc(250, 200, 225, 0, Math.PI);
+      ctx.fill();
+      break;
+    default:
+      break;
+  }
 }
 
 $(document).ready(function(){
+  var paintColor = '#bd354d'; //#ff9696
+  $('.slider-fill').css('background-color', paintColor);
+
   var $canvas = $('#game');
   var ctx = getContext($canvas);
 
   ctx.lineWidth = 15;
   ctx.lineJoin = ctx.lineCap = 'round';
-  ctx.strokeStyle = '#bd354d'; //#ff9696
+  ctx.strokeStyle = paintColor;
 
   var isDrawing, strokes = [];
+  setupLevel(1);
 
   $canvas.on('mousedown', function(e) {
     isDrawing = true;
@@ -63,9 +93,15 @@ $(document).ready(function(){
   $canvas.on('mousemove', function(e) {
     if (!isDrawing) return;
 
-    strokes[strokes.length-1].push(getCursorPos($canvas[0], e));
+    var lastPos = strokes[strokes.length-1].slice(-1)[0];
+    var curPos = getCursorPos($canvas[0], e);
 
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    if (getDistance(lastPos, curPos) < 2) return;
+
+    strokes[strokes.length-1].push(curPos);
+
+    setupLevel(1);
+    //ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     
     var total = 0;
     for (var j = 0; j < strokes.length; j++) {
@@ -87,18 +123,19 @@ $(document).ready(function(){
       ctx.lineTo(p1.x, p1.y);
       ctx.stroke();
     }
-    //if(total%25===0) updatePercentPainted();
+    if(total%25===0) updatePercentAsync();
   });
 
   $(document).on('mouseup', function() {
     isDrawing = false;
-    if (strokes[strokes.length-1].length > 0) strokes.push([]);
+    if (strokes.length && strokes[strokes.length-1].length > 0) strokes.push([]);
     updatePercentPainted();
   });
 
   $('#eraser').click(function() {
     strokes.length = 0;
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    setupLevel(1);
+    //ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     updatePercentPainted();
   });
 });
